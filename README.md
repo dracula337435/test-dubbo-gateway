a6573ff0e1c1b57a61b74f761221d60b56ea8936
dk <dracula337435@163.com>
2019-01-10 19:53:49 +0800

    jdk动态代理，成功

--- README.md
+++ README.md
@@ -6,4 +6,5 @@
 1. 直接把reference注进service，运行时错误，gateway方报错```com.alibaba.dubbo.common.bytecode.NoSuchMethodException: Method [sayHello] not found.```  
 见版本```69d10ea8 on 2019/1/10 at 19:14```
 1. 用dubbo泛化实现，运行时错误```java.lang.IllegalArgumentException: argument type mismatch```  
-见```generic```项目中的例子，dubbo泛化```GenericService```传入参数，当复杂对象时就成了```Map```，再转换一次的话太麻烦
+见```generic```项目中的例子，dubbo泛化```GenericService```传入参数，当复杂对象时就成了```Map```，再转换一次的话太麻烦  
+见版本```24a91938 on 2019/1/10 at 19:18```
---
+++ gateway/src/test/java/org/dracula/test/dubbo/gateway/gateway/FakeImpl.java
@@ -0,0 +1,34 @@
+package org.dracula.test.dubbo.gateway.gateway;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author dk
+ */
+public class FakeImpl {
+
+    public static Object getObject(Class objectType, Object ref) {
+        return Proxy.newProxyInstance(objectType.getClassLoader(), new Class[]{objectType}, new MyInvocationHandler(ref));
+    }
+
+    /**
+     * @author dk
+     */
+    public static class MyInvocationHandler implements InvocationHandler{
+
+        private Object ref;
+
+        public MyInvocationHandler(Object ref) {
+            this.ref = ref;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            return method.invoke(ref, args);
+        }
+
+    }
+
+}
