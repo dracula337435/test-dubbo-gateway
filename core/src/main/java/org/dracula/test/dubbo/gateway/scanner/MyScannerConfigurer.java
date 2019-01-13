e5ab953751f0ab6d96425f1d8c8c9366251273b4
dracula337435 <dracula337435@163.com>
2019-01-14 01:39:52 +0800

    分离出ReferenceConfig和ServiceConfig，成为模板。参考dubbo自有的从注解向Config转换的方法。相应地，修改示例

--- core/src/main/java/org/dracula/test/dubbo/gateway/scanner/MyScannerConfigurer.java
+++ core/src/main/java/org/dracula/test/dubbo/gateway/scanner/MyScannerConfigurer.java
@@ -1,9 +1,18 @@
 package org.dracula.test.dubbo.gateway.scanner;
 
+import com.alibaba.dubbo.common.utils.CollectionUtils;
+import com.alibaba.dubbo.common.utils.ReflectUtils;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.dubbo.config.spring.ReferenceBean;
 import com.alibaba.dubbo.config.spring.ServiceBean;
 import org.dracula.test.dubbo.gateway.nowhere.FakeImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.config.*;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
@@ -12,6 +21,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 import org.springframework.util.StringUtils;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -22,18 +34,25 @@ import java.util.function.Consumer;
  */
 public class MyScannerConfigurer implements BeanDefinitionRegistryPostProcessor {
 
+    private static Logger logger = LoggerFactory.getLogger(MyScannerConfigurer.class);
+
     private String basePackage;
 
     private Map<String, String> class2remote = new HashMap<>();
 
     private Map<String, String> class2nowhere = new HashMap<>();
 
+    private ServiceConfig serviceConfigTemplate;
+
+    private ReferenceConfig referenceConfigTemplate;
+
     private Consumer<BeanDefinitionHolder> scan1st = holder -> {
         GenericBeanDefinition definition = (GenericBeanDefinition) holder.getBeanDefinition();
         String previousClassName = definition.getBeanClassName();
         definition.setBeanClass(ReferenceBean.class);
-        definition.getPropertyValues().addPropertyValue("interface", previousClassName);
-        definition.getPropertyValues().addPropertyValue("url", "rest://localhost:8080");
+        MutablePropertyValues propertyValues = definition.getPropertyValues();
+        propertyValues.addPropertyValue("interface", previousClassName);
+        appendAnnotation(Reference.class, referenceConfigTemplate, propertyValues);
     };
 
     private Consumer<BeanDefinitionHolder> scan2nd = holder -> {
@@ -49,8 +68,11 @@ public class MyScannerConfigurer implements BeanDefinitionRegistryPostProcessor 
         GenericBeanDefinition definition = (GenericBeanDefinition) holder.getBeanDefinition();
         String previousClassName = definition.getBeanClassName();
         definition.setBeanClass(ServiceBean.class);
-        definition.getPropertyValues().add("interface", previousClassName);
-        definition.getPropertyValues().add("ref", new RuntimeBeanReference(class2nowhere.get(previousClassName)));
+        MutablePropertyValues propertyValues = definition.getPropertyValues();
+        propertyValues.add("interface", previousClassName);
+        propertyValues.add("ref", new RuntimeBeanReference(class2nowhere.get(previousClassName)));
+        //抄注解写法的话，注解中正好没有ref，想来也合理
+        appendAnnotation(Service.class, serviceConfigTemplate, propertyValues);
     };
 
     @Override
@@ -88,6 +110,70 @@ public class MyScannerConfigurer implements BeanDefinitionRegistryPostProcessor 
         // left intentionally blank
     }
 
+    /**
+     * 参考com.alibaba.dubbo.config.AbstractConfig#appendAnnotation(java.lang.Class, java.lang.Object)
+     * @param annotationClass
+     * @param annotation
+     */
+//    protected void appendAnnotation(Class<?> annotationClass, Object annotation) {
+    private void appendAnnotation(Class<?> annotationClass, Object annotation, MutablePropertyValues propertyValues) {
+        Method[] methods = annotationClass.getMethods();
+        for (Method method : methods) {
+//            if (method.getDeclaringClass() != Object.class
+            if (method.getDeclaringClass() != Annotation.class
+                    && method.getReturnType() != void.class
+                    && method.getParameterTypes().length == 0
+                    && Modifier.isPublic(method.getModifiers())
+                    && !Modifier.isStatic(method.getModifiers())) {
+                try {
+                    String property = method.getName();
+                    if ("interfaceClass".equals(property) || "interfaceName".equals(property)) {
+//                        property = "interface";
+                        continue;
+                    }
+//                    String setter = "set" + property.substring(0, 1).toUpperCase() + property.substring(1);
+                    //发现如果返回值是boolean，getter方法名用的是“is...”
+                    String getterPrefix = isBoolean(method.getReturnType()) && !isException(property) ? "is":"get";
+                    String getter = getterPrefix + property.substring(0, 1).toUpperCase() + property.substring(1);
+                    Method getterMethod = annotation.getClass().getMethod(getter);
+//                    Object value = method.invoke(annotation, new Object[0]);
+                    Object value = getterMethod.invoke(annotation);
+                    if (value != null && !value.equals(method.getDefaultValue())) {
+                        Class<?> parameterType = ReflectUtils.getBoxedClass(method.getReturnType());
+                        if ("filter".equals(property) || "listener".equals(property)) {
+                            parameterType = String.class;
+                            value = com.alibaba.dubbo.common.utils.StringUtils.join((String[]) value, ",");
+                        } else if ("parameters".equals(property)) {
+                            parameterType = Map.class;
+                            value = CollectionUtils.toStringMap((String[]) value);
+                        }
+//                        try {
+//                            Method setterMethod = getClass().getMethod(setter, new Class<?>[]{parameterType});
+//                            setterMethod.invoke(this, new Object[]{value});
+//                        } catch (NoSuchMethodException e) {
+//                            // ignore
+//                        }
+                        propertyValues.addPropertyValue(property, value);
+                    }
+                } catch (Throwable e) {
+                    logger.error(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    private boolean isBoolean(Class clazz){
+        return Boolean.class==clazz || boolean.class==clazz;
+    }
+
+    private boolean isException(String prop){
+        return "lazy".equals(prop)
+                || "sticky".equals(prop)
+                || "sent".equals(prop)
+                || "stubevent".equals(prop)
+                || "export".equals(prop);
+    }
+
     public String getBasePackage() {
         return basePackage;
     }
@@ -96,4 +182,19 @@ public class MyScannerConfigurer implements BeanDefinitionRegistryPostProcessor 
         this.basePackage = basePackage;
     }
 
+    public ServiceConfig getServiceConfigTemplate() {
+        return serviceConfigTemplate;
+    }
+
+    public void setServiceConfigTemplate(ServiceConfig serviceConfigTemplate) {
+        this.serviceConfigTemplate = serviceConfigTemplate;
+    }
+
+    public ReferenceConfig getReferenceConfigTemplate() {
+        return referenceConfigTemplate;
+    }
+
+    public void setReferenceConfigTemplate(ReferenceConfig referenceConfigTemplate) {
+        this.referenceConfigTemplate = referenceConfigTemplate;
+    }
 }
