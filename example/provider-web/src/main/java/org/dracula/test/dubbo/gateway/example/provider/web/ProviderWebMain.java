3f52fe8fdb19cf450b5ead629765e44a00447c61
dracula337435 <dracula337435@163.com>
2019-01-13 23:19:22 +0800

    用web方式提供服务

---
+++ example/provider-web/src/main/java/org/dracula/test/dubbo/gateway/example/provider/web/ProviderWebMain.java
@@ -0,0 +1,20 @@
+package org.dracula.test.dubbo.gateway.example.provider.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dk
+ */
+@SpringBootApplication
+public class ProviderWebMain {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        SpringApplication.run(ProviderWebMain.class, args);
+    }
+
+}
