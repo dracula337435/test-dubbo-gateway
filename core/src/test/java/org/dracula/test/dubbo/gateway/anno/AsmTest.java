cdc14aa247f8fb30b6c50e887c7fb6f89b269b55
dracula337435 <dracula337435@163.com>
2019-01-13 22:57:33 +0800

    修正读文件的错误，增加注释

--- core/src/test/java/org/dracula/test/dubbo/gateway/anno/AsmTest.java
+++ core/src/test/java/org/dracula/test/dubbo/gateway/anno/AsmTest.java
@@ -6,20 +6,26 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
-
+/**
+ * @author dk
+ */
 public class AsmTest {
 
     @Test
     public void test() throws Exception {
+        //
         InputStream fis = AsmTest.class.getClassLoader().getResourceAsStream("org\\dracula\\test\\dubbo\\gateway\\anno\\InterfaceWithoutAnnotation.class");
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        //
         byte[] buffer = new byte[4096];
         int len = -1;
         while((len = fis.read(buffer)) != -1){
-            byteArrayOutputStream.write(buffer);
+            byteArrayOutputStream.write(buffer, 0, len);
         }
+        byte[] classFile = byteArrayOutputStream.toByteArray();
+        //
         MyClassFileByteTransformer myClassFileByteTransformer = new MyClassFileByteTransformer();
-        byte[] newClassFile = myClassFileByteTransformer.transform(byteArrayOutputStream.toByteArray());
+        byte[] newClassFile = myClassFileByteTransformer.transform(classFile);
         FileOutputStream fos = new FileOutputStream("E:\\tmp\\InterfaceWithoutAnnotation.class");//覆盖当前class文件
         fos.write(newClassFile);
         fos.close();
