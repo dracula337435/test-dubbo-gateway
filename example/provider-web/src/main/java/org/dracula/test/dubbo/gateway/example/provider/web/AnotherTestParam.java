2480a608b84b7e6802395971b8974f46e86cefe1
dracula337435 <dracula337435@163.com>
2019-01-13 23:45:55 +0800

    dubbo使用url直连，服务提供方使用controller。controller完全没使用dubbo用的接口。

---
+++ example/provider-web/src/main/java/org/dracula/test/dubbo/gateway/example/provider/web/AnotherTestParam.java
@@ -0,0 +1,17 @@
+package org.dracula.test.dubbo.gateway.example.provider.web;
+
+/**
+ * @author dk
+ */
+public class AnotherTestParam {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ example/provider-web/src/main/java/org/dracula/test/dubbo/gateway/example/provider/web/TestController.java
@@ -0,0 +1,27 @@
+package org.dracula.test.dubbo.gateway.example.provider.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dk
+ */
+@RestController
+@RequestMapping("/TestInterface")
+public class TestController {
+
+    @PostMapping("/sayHello")
+    public AnotherTestParam sayHello(@RequestBody AnotherTestParam param){
+        param.setText("in web, hello "+param.getText());
+        return param;
+    }
+
+    @PostMapping("/sayHello2")
+    public AnotherTestParam sayHello2(@RequestBody AnotherTestParam param){
+        param.setText("in web, hello 2 "+param.getText());
+        return param;
+    }
+
+}
